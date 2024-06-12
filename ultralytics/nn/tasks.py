2821aaac7b724bc494026bfbd46b2cfc4fe6983b
haitamrarhai <120592255+haitamrarhai@users.noreply.github.com>
2024-06-12 14:19:20 +0200

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -49,6 +49,8 @@ from ultralytics.nn.modules import (
     Segment,
     Silence,
     WorldDetect,
+    EMA,
+    ResBlock_CBAM,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -885,6 +887,8 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             DWConvTranspose2d,
             C3x,
             RepC3,
+            EMA,
+            ResBlock_CBAM,
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
