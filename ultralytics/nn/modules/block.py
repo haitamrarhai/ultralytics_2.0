122e96f43dbe49e7add61d4c10e73d6256dc2846
haitamrarhai <120592255+haitamrarhai@users.noreply.github.com>
2024-06-26 11:58:26 +0200

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -698,46 +698,46 @@ class EMA(nn.Module):
 
     def forward(self, x):
         b, c, h, w = x.size()
-        print(f'Input shape: {x.shape}')
+        #print(f'Input shape: {x.shape}')
         
         group_x = x.reshape(b * self.groups, -1, h, w)  # b*g, c//g, h, w
-        print(f'Group shape: {group_x.shape}')
+        #print(f'Group shape: {group_x.shape}')
         
         x_h = self.pool_h(group_x)
-        print(f'Pool_h shape: {x_h.shape}')
+        #print(f'Pool_h shape: {x_h.shape}')
         
         x_w = self.pool_w(group_x).permute(0, 1, 3, 2)
-        print(f'Pool_w shape: {x_w.shape}')
+        #print(f'Pool_w shape: {x_w.shape}')
         
         hw = self.conv1x1(torch.cat([x_h, x_w], dim=2))
-        print(f'After conv1x1 cat: {hw.shape}')
+        #print(f'After conv1x1 cat: {hw.shape}')
         
         x_h, x_w = torch.split(hw, [h, w], dim=2)
-        print(f'After split: x_h shape: {x_h.shape}, x_w shape: {x_w.shape}')
+        #print(f'After split: x_h shape: {x_h.shape}, x_w shape: {x_w.shape}')
         
         x1 = self.gn(group_x * x_h.sigmoid() * x_w.permute(0, 1, 3, 2).sigmoid())
-        print(f'After gn and sigmoid: {x1.shape}')
+        #print(f'After gn and sigmoid: {x1.shape}')
         
         x2 = self.conv3x3(group_x)
-        print(f'After conv3x3: {x2.shape}')
+        #print(f'After conv3x3: {x2.shape}')
         
         x11 = self.softmax(self.agp(x1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
-        print(f'After softmax x11: {x11.shape}')
+        #print(f'After softmax x11: {x11.shape}')
         
         x12 = x2.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
-        print(f'Reshaped x12: {x12.shape}')
+        #print(f'Reshaped x12: {x12.shape}')
         
         x21 = self.softmax(self.agp(x2).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
-        print(f'After softmax x21: {x21.shape}')
+        #print(f'After softmax x21: {x21.shape}')
         
         x22 = x1.reshape(b * self.groups, c // self.groups, -1)  # b*g, c//g, hw
-        print(f'Reshaped x22: {x22.shape}')
+        #print(f'Reshaped x22: {x22.shape}')
         
         weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(b * self.groups, 1, h, w)
-        print(f'Weights shape: {weights.shape}')
+        #print(f'Weights shape: {weights.shape}')
         
         output = (group_x * weights.sigmoid()).reshape(b, c, h, w)
-        print(f'Output shape: {output.shape}')
+        #print(f'Output shape: {output.shape}')
         
         return output
 
